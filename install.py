dceed4c55d96d1a862fdc79beaa45b896d28be0a
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-11-20 13:16:21 +0000

    Update install.py

--- install.py
+++ install.py
@@ -92,7 +92,61 @@ runIfPathMissing("/usr/share/doc/zlib1g-dev", "apt-get install -y zlib1g-dev")
 #runIfPathMissing("/usr/local/lib/"+pythonVersion+"/dist-packages/flask", "pip3 install flask")
 
 # Make sure Expect (command-line automation utility) is installed.
-runIfPathMissing("/usr/bin/expect", "apt-get -y install expect")
+#runIfPathMissing("/usr/bin/expect", "apt-get -y install expect")
+
+# Make sure Cairo (library used by Guacamole for rendering graphics) is installed.
+runIfPathMissing("/usr/share/doc/libcairo2-dev", "apt-get install -y libcairo2-dev")
+
+# Make sure libjpeg62-turbo-dev (library used by Guacamole for handling JPEG image data) is installed.
+runIfPathMissing("/usr/share/doc/libjpeg62-turbo-dev", "apt-get install -y libjpeg62-turbo-dev")
+
+# Make sure libpng-dev (library used by Guacamole for handling PNG image data) is installed.
+runIfPathMissing("/usr/share/doc/libpng-dev", "apt-get install -y libpng-dev")
+
+# Make sure libossp-uuid-dev (library used by Guacamole for handling PNG image data) is installed.
+runIfPathMissing("/usr/share/doc/libossp-uuid-dev", "apt-get install -y libossp-uuid-dev")
+
+# Make sure FFmpeg (library used by Guacamole for handling video encoding) is installed.
+runIfPathMissing("/usr/share/doc/libavcodec-dev", "apt-get install -y libavcodec-dev")
+runIfPathMissing("/usr/share/doc/libavutil-dev", "apt-get install -y libavutil-dev")
+runIfPathMissing("/usr/share/doc/libswscale-dev", "apt-get install -y libswscale-dev")
+
+# Make sure FreeRDP (library used by Guacamole to handle RDP connections) is installed.
+runIfPathMissing("/usr/share/doc/libfreerdp-dev", "apt-get install -y libfreerdp-dev")
+
+# Make sure Pango (library used by Guacamole to support SSH and Telnet connections) is installed.
+runIfPathMissing("/usr/share/doc/libpango1.0-dev", "apt-get install -y libpango1.0-dev")
+
+# Make sure libssh2 (library used by Guacamole to support SSH connections) is installed.
+runIfPathMissing("/usr/share/doc/libssh2-1-dev", "apt-get install -y libssh2-1-dev")
+
+# Make sure libtelnet (library used by Guacamole to support Telnet connections) is installed.
+runIfPathMissing("/usr/share/doc/libtelnet-dev", "apt-get install -y libtelnet-dev")
+
+# Make sure libvncserver (library used by Guacamole to support VNC connections) is installed.
+runIfPathMissing("/usr/share/doc/libvncserver-dev", "apt-get install -y libvncserver-dev")
+
+# Make sure Pulse Audio (library used by Guacamole to support audio over VNC) is installed.
+runIfPathMissing("/usr/share/doc/libpulse-dev", "apt-get install -y libpulse-dev")
+
+# Make sure libssl (library used by Guacamole to support SSL) is installed.
+runIfPathMissing("/usr/share/doc/libssl-dev", "apt-get install -y libssl-dev")
+
+# Make sure Ogg Vorbis (library used by Guacamole to support Ogg Vorbis audio) is installed.
+runIfPathMissing("/usr/share/doc/libvorbis-dev", "apt-get install -y libvorbis-dev")
+
+# Make sure libwebp (library used by Guacamole to support WebP image data) is installed.
+runIfPathMissing("/usr/share/doc/libwebp-dev", "apt-get install -y libwebp-dev")
+
+# Make sure the Tomcat servlet container is installed (used for serving Guacamole client contents to users connecting to the Guacamole server via web).
+runIfPathMissing("/usr/share/doc/tomcat8", "apt-get install -y tomcat8 tomcat8-admin tomcat8-common tomcat8-user")
+
+# Make sure the Nginx web/proxy server is installed (used to proxy the Tomcat server and provide SSL)...
+runIfPathMissing("/usr/share/doc/nginx", "apt-get install -y nginx")
+# ...with support for Let's Encrypt. See here:
+# https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-debian-9
+# Also, see later section on crontab for daily certbot renew / backup process.
+#runIfPathMissing("/usr/lib/python3/dist-packages/certbot", "apt-get install -y python-certbot-nginx -t stretch-backports")
 
 sys.exit(0)
 
