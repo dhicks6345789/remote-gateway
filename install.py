561c09cbdf207c57caca90d03503b40f65fbe304
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-12-05 16:39:46 +0000

    Update install.py

--- install.py
+++ install.py
@@ -201,6 +201,8 @@ if not os.path.exists("/usr/share/doc/mariadb-server/BANANAS"):
         "send \"n\\r\"",
         "expect \"\\[Y/n\\]\"",
         "send \"y\\r\"",
+        "expect \"\\[Y/n\\]\"",
+        "send \"y\\r\"",
         
         "interact"
     ])
