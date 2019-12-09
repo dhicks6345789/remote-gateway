c72bddf399720968029159cb2451bdb375ca6a35
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-12-09 16:03:20 +0000

    Update install.py

--- install.py
+++ install.py
@@ -98,7 +98,7 @@ runIfPathMissing("/usr/share/doc/zlib1g-dev", "apt-get install -y zlib1g-dev")
 runIfPathMissing("/usr/bin/expect", "apt-get -y install expect")
 
 # Make sure the Java Development Kit (JDK, used fro compiling Java applications) is installed.
-runIfPathMissing("/usr/bin/default-jdk", "apt-get install -y default-jdk")
+runIfPathMissing("/usr/share/doc/default-jdk", "apt-get install -y default-jdk")
 
 
 
