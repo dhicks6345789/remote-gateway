5a5cc73ec2771a2a82c7a920277f7963dd50a684
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2019-12-11 15:37:22 +0000

    Update MystartAuthenticationProvider.java

--- MystartAuthenticationProvider.java
+++ MystartAuthenticationProvider.java
@@ -1,5 +1,8 @@
 package org.apache.guacamole.auth;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 import java.util.HashMap;
