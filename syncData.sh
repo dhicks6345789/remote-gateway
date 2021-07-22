c92942fc596b010ec19f2f4584f2d94d75f0337d
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2021-07-22 18:03:27 +0100

    Update syncData.sh

--- syncData.sh
+++ syncData.sh
@@ -2,6 +2,7 @@ mkdir /etc/guacamole/connections > /dev/null 2>&1
 mkdir /etc/guacamole/groups > /dev/null 2>&1
 
 echo STATUS: Syncing data from Google Drive...
-rclone --config .config\rclone\rclone.conf sync "drive:connections" "/etc/guacamole/connections" 2>&1
+rclone --config /root/.config/rclone/rclone.conf sync "drive:Connections" "/etc/guacamole/connections" 2>&1
+rclone --config /root/.config/rclone/rclone.conf sync "drive:Groups" "/etc/guacamole/groups" 2>&1
 
 echo STATUS: Done.
