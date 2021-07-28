44dbfb4fe82afa581b4e6290beb4e30d1cec91d5
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2021-07-28 15:11:57 +0100

    Update api.py

--- api.py
+++ api.py
@@ -87,7 +87,7 @@ def api():
             host = hosts[connection[0].lower()]
             os.system("sshpass -p " + host[2] + " ssh -o \"StrictHostKeyChecking=no\" " + host[0] + " " + host[3].replace("<<KEY>>",loginToken))
             xmlData = xmlData + "\t\t<connection name=\"" + connection[0] + "\">\n"
-            xmlData = xmlData + "\t\t\t<protocol>" + host[1] + "</protocol>\n"
+            xmlData = xmlData + "\t\t\t<protocol>" + host[1].lower() + "</protocol>\n"
             xmlData = xmlData + "\t\t\t<param name=\"hostname\">" + host[0].split("@")[1] + "</param>\n"
             xmlData = xmlData + "\t\t\t<param name=\"port\">5900</param>\n"
             xmlData = xmlData + "\t\t\t<param name=\"password\">" + loginToken + "</param>\n"
