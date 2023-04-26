6e43231df8f093578bada6614b4c337485f3b496
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-04-26 18:20:15 +0100

    Delete syncData.sh

--- syncData.sh
+++
@@ -1,9 +0,0 @@
-mkdir /etc/guacamole/connections > /dev/null 2>&1
-mkdir /etc/guacamole/groups > /dev/null 2>&1
-
-echo STATUS: Syncing data from Google Drive...
-rclone --config /root/.config/rclone/rclone.conf sync "drive:Connections" "/etc/guacamole/connections" 2>&1
-rclone --config /root/.config/rclone/rclone.conf sync "drive:Groups" "/etc/guacamole/groups" 2>&1
-rclone --config /root/.config/rclone/rclone.conf sync "drive:Hosts" "/etc/guacamole/hosts" 2>&1
-
-echo STATUS: Done.
