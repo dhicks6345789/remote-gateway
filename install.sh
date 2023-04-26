b28de2dbae6190411248b282e6b5b6f2132ee93e
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-04-26 13:24:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
-while getopts servername:databasepw:guacpw: flag
-do
-  case "${flag}" in
-    servername) servername=${OPTARG};;
-    databasepw) databasepw=${OPTARG};;
-    guacpw) guacpw=${OPTARG};;
-  esac
+while test $# -gt 0; do
+    case "$1" in
+        -servername)
+            shift
+            servername=$1
+            shift
+            ;;
+        -databasepw)
+            shift
+            databasepw=$1
+            shift
+            ;;
+        -guacpw)
+            shift
+            guacpw=$1
+            shift
+            ;;
+        *)
+            echo "$1 is not a recognized flag!"
+            return 1;
+            ;;
+    esac
 done
+
 echo "Servername: $servername";
 echo "Database password: $databasepw";
 echo "Guacamole password: $guacpw";
