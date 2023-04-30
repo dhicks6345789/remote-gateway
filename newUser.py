001748b32a4f775e3a142aec4ae1c29920d86cb4
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-04-30 14:17:56 +0100

    Update newUser.py

--- newUser.py
+++ newUser.py
@@ -30,7 +30,7 @@ def runAndLog(theParameters):
 
 # Run the "create user" command remotely via SSH on a Windows server. You'll need to set up SSH Server on your Windows server (now included as a standard
 # component of Windows) and set up a private key for authorisation. Replace the IP address in the line below with the address / name of your server.
-runAndLog(["ssh", "-i, "/var/www/id_rsa", "administrator@192.168.1.112", "-t", "\"net user " + username + " " + password + " /ADD /Y\"", "2>&1"])
+runAndLog(["ssh", "-i", "/var/www/id_rsa", "administrator@192.168.1.112", "-t", "\"net user " + username + " " + password + " /ADD /Y\"", "2>&1"])
 #runAndLog("ssh", "-i /var/www/id_rsa administrator@192.168.1.112 -t \"net localgroup 'Remote Desktop Users' " + username + " /add\" 2>&1")
 # Return the new username and password to the calling CGI script.
 print(username + "," + password)
