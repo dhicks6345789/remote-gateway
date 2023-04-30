6c23b401b19030420670648c32eff8dc73ae5d10
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-04-30 16:53:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,6 +114,7 @@ chown www-data /etc/guacamole/user-mapping.xml
 if [ ! -d "/var/www/.ssh" ]; then
     mkdir /var/www/.ssh
     chown www-data:www-data /var/www/.ssh
+    chmod 700 /var/www/.ssh
 fi
 
 
