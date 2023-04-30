dd49e16ad400faea2d431c435d7fc94e6e5a7208
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-04-30 13:22:15 +0100

    Update newUser.py

--- newUser.py
+++ newUser.py
@@ -2,23 +2,25 @@
 # set up to allow all users in your domain to log in but you don't want to have to pre-create accounts for every user onyour domain, you can have this script
 # create accounts as needed. Uncomment the section you want to use.
 
+import sys
+import logging
+
+# Get parameters passed from the CGI script.
+username = sys.argv[1]
+password = sys.argv[2]
+
+# Set up logging.
+logging.basicConfig(filename="/var/log/remote-gateway/remote-gateway.log", encoding="utf-8", format="%(asctime)s - %(message)s", level=logging.INFO)
 
 
-# Default behaviour - do nothing, return an empty string.
+
+# Default behaviour - log the login attempt, return a blank value.
+logging.info("User denined access: " + username)
 print("")
 
 
 
-## Use SSH (athorised with a private key) to create a new user on a Windows server.
-#import sys
-#import logging
-#
-## Get parameters passed from the CGI script.
-#username = sys.argv[1]
-#password = sys.argv[2]
-## Set up logging.
-#logging.basicConfig(filename="/var/log/remote-gateway/remote-gateway.log", encoding="utf-8", format="%(asctime)s - %(message)s", level=logging.INFO)
-## Run the "create user" command remotely via SSH on the Windows server. You'll need to set up SSH Server on your Windows server (now included as a standard
+## Run the "create user" command remotely via SSH on a Windows server. You'll need to set up SSH Server on your Windows server (now included as a standard
 ## component of Windows) and set up a private key for authorisation. Replace the IP address in the line below with the address / name of your server.
 #os.system("ssh -i /root/.ssh/id_rsa administrator@192.168.1.112 -t \"net user " + username + " " + password " /add\"")
 ## Log what we've done.
