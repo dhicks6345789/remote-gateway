cc2a4fe184613128f938e1a12d59b6d639dd968f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2023-06-14 17:12:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,12 +41,10 @@ fi
 
 # Use Itiligent's script to install a Guacamole server - see: https://github.com/itiligent/Guacamole-Setup
 if [ ! -d "/etc/guacamole" ]; then
-    #copyOrDownload 1-setup.sh 1-setup.sh 0755
-    #./1-setup.sh
-    #rm 1-setup.sh
-    #rm -rf guac-setup
-    apt install -y guacamole-tomcat
-    apt install -y libguac-client-ssh0 libguac-client-rdp0
+    copyOrDownload 1-setup.sh 1-setup.sh 0755
+    ./1-setup.sh
+    rm 1-setup.sh
+    rm -rf guac-setup
 fi
 ## For now:
 #if [ -f "/etc/guacamole/extensions/guacamole-auth-jdbc-mysql-1.5.0.jar" ]; then
